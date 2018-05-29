ebe2215f19fa683446e142842f0d001c203d3b1b
Simon Bay <simon.bay@orange.fr>
2018-05-29 15:30:00 +0200

    Classe MenuTxt codée

--- src/arcanor/iu/console/MenuTxt.java
+++ src/arcanor/iu/console/MenuTxt.java
@@ -1,7 +1,5 @@
 package arcanor.iu.console;
 
-import java.util.Scanner;
-
 /** permet d'afficher le menu sur la console
 * @author G.Renault, M.Poiré, S.Bay, M.Racinne-Divet */
 public class MenuTxt {
@@ -12,17 +10,7 @@ public class MenuTxt {
       System.out.println("\t(1.) Nouvelle partie");
       System.out.println("\t(2.) Charger");
       System.out.println("\t(3.) Quitter");
-      return selection();
-  }
-
-  public static int selection (){
-      Scanner sc = new Scanner (System.in);
-      System.out.println("Saisissez votre choix :");
-      int ret = 0;
-      while(ret !=1 && ret !=2 && ret !=3){
-          ret = sc.nextInt();
-      }
-      return ret;
+      return arcanor.controleur.console.ConsoleEcout.menuTxtEcout();
   }
 
 }

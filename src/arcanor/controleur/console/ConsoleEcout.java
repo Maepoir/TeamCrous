f2e3f1ef9b8d322420e4ea8865cf5809f13c871a
Simon Bay <simon.bay@orange.fr>
2018-05-29 15:30:14 +0200

    Début de ConsoleEcout

--- src/arcanor/controleur/console/ConsoleEcout.java
+++ src/arcanor/controleur/console/ConsoleEcout.java
@@ -1,7 +1,20 @@
 package arcanor.controleur.console;
 import java.awt.event.*;
+import java.util.Scanner;
+
 /** permet de mettre à l'écoute la console
 * @author G.Renault, M.Poiré, S.Bay, M.Racinne-Divet */
 public class ConsoleEcout{
+
+
+    public static int menuTxtEcout (){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Saisissez votre choix :");
+        int ret = 0;
+        while(ret !=1 && ret !=2 && ret !=3){
+            ret = sc.nextInt();
+        }
+        return ret;
+    }
 
 }

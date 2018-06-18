ed08e25f99130a8a512cb7cc86e35fa345e63279
Maepoir <34347323+Maepoir@users.noreply.github.com>
2018-06-18 16:17:07 +0200

    Menu console qui compile et lance un jeu

--- src/arcanor/iu/console/MenuTxt.java
+++ src/arcanor/iu/console/MenuTxt.java
@@ -1,16 +1,69 @@
 package arcanor.iu.console;
+import java.util.*;
+import arcanor.modele.*;
+import arcanor.*;
 
 /** permet d'afficher le menu sur la console
 * @author G.Renault, M.Poiré, S.Bay, M.Racinne-Divet */
 public class MenuTxt {
 
   /**permet d'afficher le menu sur la console */
-  public static int afficherMenu(){
+  public void afficherMenu(){
       System.out.println(" <3 Arcanor <3 ");
       System.out.println("\t(1.) Nouvelle partie");
       System.out.println("\t(2.) Charger");
       System.out.println("\t(3.) Quitter");
-      return arcanor.controleur.console.ConsoleEcout.menuTxtEcout();
+      Scanner sc = new Scanner (System.in);
+      System.out.println("Saisissez votre choix :");
+      int choix = sc.nextInt();
+      while(choix !=1 && choix !=2 && choix !=3){
+          choix = sc.nextInt();
+      }
+      if(choix == 1){
+        System.out.println("Saisir nom du joueur 1 :");
+        Scanner in = new Scanner (System.in);
+        String nom1 = in.nextLine();
+        JoueurHumain j1 = new JoueurHumain(nom1,Couleur.BLANC, true);
+        System.out.println("Jouer contre un IA ?(o/n)");
+        Joueur j2;
+        String ia = in.nextLine();
+        if(ia.equals("o")){
+          System.out.println("Saisir nom de l'IA : ");
+          String nom2 = in.nextLine();
+          j2 = new IA(nom2, Couleur.NOIR, false,1);
+        }
+        else{
+          //si la saisie est incorrecte, il s'agira d'une partie entre 2 joueurs
+          System.out.println("Saisir nom du joueur 2 : ");
+          String nom2 = in.nextLine();
+          j2 = new JoueurHumain(nom2,Couleur.NOIR, false);
+        }
+        Plateau plateau = new Plateau();
+        plateau.initPions(j1, j2);
+        boolean modeGraphique = false;
+        System.out.println("Jouer en mode triche (voir quel pion à déjà mangé un autre) ou en mode normal ? (t/n)");
+        String tricheNormal = in.nextLine();
+        if(tricheNormal.equals("t")){
+          Jeu jeu = new Jeu(j1,j2,plateau, ModeJeu.TRICHE,modeGraphique);
+        }
+        else{
+          Jeu jeu = new Jeu(j1,j2,plateau,ModeJeu.NORMAL,modeGraphique);
+        }
+
+
+
+        //garder pour Simon
+        //PlateauTxt plTxt = new PlateauTxt(plateau, j1, j2);
+      }
+      //cet condition non codée, uniquement pour la compilation
+      else if(choix == 2){int i = 0;}
+      //en cas de saisie incorrecte ou de choix 3
+      else{
+        System.out.println("Merci d'avoir lancé Arcanor, à bientôt !");
+        System.exit(0);
+      }
+
+
   }
 
 }

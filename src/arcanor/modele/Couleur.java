8c73dd2c776d6197e917ca97027bab9e64e6b5a0
Maepoir <34347323+Maepoir@users.noreply.github.com>
2018-06-18 14:38:51 +0200

    Add files via upload

---
+++ src/arcanor/modele/Couleur.java
@@ -0,0 +1,7 @@
+/** liste des couleurs de pions possibles
+* @author S.BAY, M.POIRE */
+public enum Couleur {
+  BLANC,
+  NOIR,
+  AUCUN;
+}
---
+++ src/arcanor/modele/ModeJeu.java
@@ -0,0 +1,6 @@
+/** énumère les modes de jeux possibles
+* @author S.BAY, M.POIRE */
+public enum ModeJeu {
+  TRICHE,
+  NORMAL;
+}

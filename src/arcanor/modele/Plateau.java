dd2d6579eb6e5d81a09e93dd6f31d3c92a450cad
Simon Bay <simon.bay@orange.fr>
2018-06-21 11:51:01 +0200

    Maj Plateau fonctionnelle (reste exceptions pour ne pas jouer les pions cachés)

--- src/arcanor/modele/Plateau.java
+++ src/arcanor/modele/Plateau.java
@@ -35,19 +35,19 @@ public class Plateau {
 
 //    Initialisation des pions du joueur 1
       this.lePlateau[0][0] = new Pion(2, false, new Pion(1, true, null, 0, 0, this.j1,1), 0, 0, this.j1,2);
-      this.lePlateau[1][0] = new Pion(4, false, new Pion(3, true, null, 0, 0, this.j1,3), 0, 0, this.j1,4);
-      this.lePlateau[3][0] = new Pion(2, false, new Pion(1, true, null, 0, 0, this.j1,5), 0, 0, this.j1,6);
-      this.lePlateau[4][0] = new Pion(4, false, new Pion(3, true, null, 0, 0, this.j1,7), 0, 0, this.j1,8);
-      this.lePlateau[6][0] = new Pion(2, false, new Pion(1, true, null, 0, 0, this.j1,9), 0, 0, this.j1,10);
-      this.lePlateau[7][0] = new Pion(4, false, new Pion(3, true, null, 0, 0, this.j1,11), 0, 0, this.j1,12);
+      this.lePlateau[1][0] = new Pion(4, false, new Pion(3, true, null, 1, 0, this.j1,3), 1, 0, this.j1,4);
+      this.lePlateau[3][0] = new Pion(2, false, new Pion(1, true, null, 3, 0, this.j1,5), 3, 0, this.j1,6);
+      this.lePlateau[4][0] = new Pion(4, false, new Pion(3, true, null, 4, 0, this.j1,7), 4, 0, this.j1,8);
+      this.lePlateau[6][0] = new Pion(2, false, new Pion(1, true, null, 6, 0, this.j1,9), 6, 0, this.j1,10);
+      this.lePlateau[7][0] = new Pion(4, false, new Pion(3, true, null, 7, 0, this.j1,11), 7, 0, this.j1,12);
 
 //    Initialisation des pions du joueur 2
-      this.lePlateau[0][6] = new Pion(2, false, new Pion(1, true, null, 0, 0, this.j2,13), 0, 0, this.j2,14);
-      this.lePlateau[1][6] = new Pion(4, false, new Pion(3, true, null, 0, 0, this.j2,15), 0, 0, this.j2,16);
-      this.lePlateau[3][6] = new Pion(2, false, new Pion(1, true, null, 0, 0, this.j2,17), 0, 0, this.j2,18);
-      this.lePlateau[4][6] = new Pion(4, false, new Pion(3, true, null, 0, 0, this.j2,19), 0, 0, this.j2,20);
-      this.lePlateau[6][6] = new Pion(2, false, new Pion(1, true, null, 0, 0, this.j2,21), 0, 0, this.j2,22);
-      this.lePlateau[7][6] = new Pion(4, false, new Pion(3, true, null, 0, 0, this.j2,23), 0, 0, this.j2,24);
+      this.lePlateau[0][6] = new Pion(2, false, new Pion(1, true, null, 0, 6, this.j2,13), 0, 6, this.j2,14);
+      this.lePlateau[1][6] = new Pion(4, false, new Pion(3, true, null, 1, 6, this.j2,15), 1, 6, this.j2,16);
+      this.lePlateau[3][6] = new Pion(2, false, new Pion(1, true, null, 3, 6, this.j2,17), 3, 6, this.j2,18);
+      this.lePlateau[4][6] = new Pion(4, false, new Pion(3, true, null, 4, 6, this.j2,19), 4, 6, this.j2,20);
+      this.lePlateau[6][6] = new Pion(2, false, new Pion(1, true, null, 6, 6, this.j2,21), 6, 6, this.j2,22);
+      this.lePlateau[7][6] = new Pion(4, false, new Pion(3, true, null, 7, 6, this.j2,23), 7, 6, this.j2,24);
   }
 
   /**
@@ -73,6 +73,9 @@ public class Plateau {
       if(x != -1 && y != -1){
           this.lePlateau [x][y] = lePion;
           lePion.setXY(x,y);
+          if(lePion.getAMange()!= null){
+              lePion.getAMange().setXY(x,y);
+          }
           PlateauTxt.afficherPlateau(this);
           ret = true;
       }
@@ -102,34 +105,35 @@ public class Plateau {
   */
   public int[][] deplacementPossibles(Pion lePion){
       int [][] ret = new int[8][2];
-//      Solution 1
+//      Solution 0
       if(lePion.getX()+1<8 && lePion.getY()+1<7) {
-          ret[0][0] = lePion.getX() + 1;
-          ret[0][1] = lePion.getY() + 1;
+          ret[0][0] = lePion.getX() - 1;
+          ret[0][1] = lePion.getY() - 1;
       } else {
           ret [0][0] = -1;
           ret [0][1] = -1;
       }
 
-//      Solution 2
+//      Solution 1
       if(lePion.getX()+1<8) {
-          ret[1][0] = lePion.getX() + 1;
+          ret[1][0] = lePion.getX() - 1;
           ret[1][1] = lePion.getY();
       } else {
           ret [1][0] = -1;
           ret [1][1] = -1;
       }
 
-//      Solution 3
+
+//      Solution 2
       if(lePion.getX()+1<8 && lePion.getY()-1>=0) {
-          ret[2][0] = lePion.getX() + 1;
-          ret[2][1] = lePion.getY() - 1;
+          ret[2][0] = lePion.getX() - 1;
+          ret[2][1] = lePion.getY() + 1;
       } else {
           ret [2][0] = -1;
           ret [2][1] = -1;
       }
 
-//      Solution 4
+//      Solution 3
       if(lePion.getY()+1<7) {
           ret[3][0] = lePion.getX();
           ret[3][1] = lePion.getY() + 1;
@@ -138,36 +142,37 @@ public class Plateau {
           ret [3][1] = -1;
       }
 
-//      Solution 5
+//      Solution 4
       if(lePion.getY()-1>=0) {
-          ret[4][0] = lePion.getX();
-          ret[4][1] = lePion.getY() - 1;
+          ret[4][0] = lePion.getX() + 1;
+          ret[4][1] = lePion.getY() + 1;
       } else {
           ret [4][0] = -1;
           ret [4][1] = -1;
       }
 
-//      Solution 6
+
+//      Solution 5
       if(lePion.getX()-1>=0 && lePion.getY()+1<7) {
-          ret[5][0] = lePion.getX() - 1;
-          ret[5][1] = lePion.getY() + 1;
+          ret[5][0] = lePion.getX() + 1;
+          ret[5][1] = lePion.getY();
       } else {
           ret [5][0] = -1;
           ret [5][1] = -1;
       }
 
-//      Solution 7
+//      Solution 6
       if(lePion.getX()-1>=0) {
-          ret[6][0] = lePion.getX() - 1;
-          ret[6][1] = lePion.getY();
+          ret[6][0] = lePion.getX() + 1;
+          ret[6][1] = lePion.getY() - 1;
       } else {
           ret [6][0] = -1;
           ret [6][1] = -1;
       }
 
-//      Solution 8
+//      Solution 7
       if(lePion.getX()-1>=0 && lePion.getY()-1>=0) {
-          ret[7][0] = lePion.getX() - 1;
+          ret[7][0] = lePion.getX();
           ret[7][1] = lePion.getY() - 1;
       } else {
           ret [7][0] = -1;

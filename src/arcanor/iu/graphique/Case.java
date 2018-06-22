2ecc7efdc9a31641c631b3d0c2dbe29551ca36a2
Simon Bay <simon.bay@orange.fr>
2018-06-22 11:48:04 +0200

    MAJ Graphique Pion Case Plateau

--- src/arcanor/iu/graphique/Case.java
+++ src/arcanor/iu/graphique/Case.java
@@ -1,8 +1,22 @@
 package arcanor.iu.graphique;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 /** represente une case du plateau
 * @author G.Renault, M.Poiré, S.Bay, M.Racinne-Divet */
 public class Case extends JPanel {
+
+    public Case (){
+        try {
+            BufferedImage caseM = ImageIO.read(new File("../doc/caseM.png"));
+            add(new JLabel(new ImageIcon(caseM)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
